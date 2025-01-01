4d99cb025973479acc2eb18faa6d98c4c7eda29c
lalithranga <wkdlkumara@gmail.com>
2025-01-01 20:15:44 +1300

    First ommit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,10 @@
 import product from "../models/product'js";
 import express from "express";
 
-export.function 
+export function getAllProducts(req, res) {
+    product.find().then((products) => {
+        res.send(products)
+    }).catch((error) => {
+        res.status(400).send({ message: error.message })
+    })
+}
